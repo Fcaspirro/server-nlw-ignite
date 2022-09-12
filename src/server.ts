a789fd2c4aad3f961535a2b347706aa49d142579
Barros, Yuri <2165225@cognizant.com>
2022-09-12 10:36:54 -0300

    chore: configure lint-staged

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ const app = express()
 
 app.get('/ads', (req, resp) => {
   return resp.json([
-    {id: 1, name: "anuncio 1 "},
+    {id: 1, name: "anuncio 1"},
     {id: 2, name: "anuncio 2"},
     {id: 3, name: "anuncio 3"}
   ])
