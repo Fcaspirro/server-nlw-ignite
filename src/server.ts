6ad05d4c6726f4e8d7618523eebf152b4c72b33a
Yuri Witney <yuriwfbarros@gmail.com>
2022-09-15 10:04:30 -0300

    feat: start defaulting routes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,20 @@
-import express from "express";
+import express from 'express'
 
 const app = express()
 
+app.get('/games', (request, response) => {
+  return response.json([])
+})
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([])
+})
+
 app.get('/ads', (req, resp) => {
   return resp.json([
-    {id: 1, name: "anuncio 1"},
-    {id: 2, name: "anuncio 2"},
-    {id: 3, name: "anuncio 3"}
+    { id: 1, name: 'anuncio 1' },
+    { id: 2, name: 'anuncio 2' },
+    { id: 3, name: 'anuncio 3' }
   ])
 })
 
